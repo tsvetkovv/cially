d001b1143f130e7492d832fe552c6befd6cbb47a
skellgreco <63358722+skellgreco@users.noreply.github.com>
2025-03-22 18:35:18 +0200

    Initial Bot Setup

--- cially-bot/index.js
+++ cially-bot/index.js
@@ -1,1 +1,19 @@
-console.log('test')
+// Package Imports
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '.env') })
+
+// Variable Imports
+const token = process.env.TOKEN
+
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    
+});
+
+// Log in to Discord with your client's token
+console.log(token)
+client.login(token);
