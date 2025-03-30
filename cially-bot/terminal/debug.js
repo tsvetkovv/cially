c39b86b7ab0ba97e493b1ca042e525c0ee00d6bb
skellgreco <tixisb@gmail.com>
2025-03-30 22:30:32 +0300

    Added a toggle to enable/disable debugging in .env

--- cially-bot/terminal/debug.js
+++ cially-bot/terminal/debug.js
@@ -1,6 +1,14 @@
+const fs = require("node:fs");
+const path = require("node:path");
+require("dotenv").config({ path: path.resolve(__dirname, ".env") });
+
+const debugging_status = process.env.DEBUGGING
+
 // Simple Script to display prettier terminal messages
 function debug({ text }) {
-    console.log(`\n[DEBUG] `.yellow + `${text}`);
+    if (debugging_status == "TRUE") {
+        console.log(`\n[DEBUG] `.yellow + `${text}`);
+    }
 }
 
 module.exports = { debug };
