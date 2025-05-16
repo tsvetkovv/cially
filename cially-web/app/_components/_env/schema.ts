553de9c70d83eb9b00a3a709c441608124925bd1
Viktor Tsvetkov <11594423+tsvetkovv@users.noreply.github.com>
2025-05-16 16:26:35 +0200

    fix docker build

--- cially-web/app/_components/_env/schema.ts
+++ cially-web/app/_components/_env/schema.ts
@@ -2,8 +2,9 @@ import {z} from "zod";
 
 export const envSchema = z.object({
     // you might want to use `z.string().url()` for URLs
-    NEXT_PUBLIC_WEBSITE_URL: z.string(),
-    NEXT_PUBLIC_BOT_API_URL: z.string()
+    // optional because they don't exist during pre-render (next build command for Docker)
+    NEXT_PUBLIC_WEBSITE_URL: z.string().optional(),
+    NEXT_PUBLIC_BOT_API_URL: z.string().optional()
 });
 
 export type EnvSchema = z.infer<typeof envSchema>;
